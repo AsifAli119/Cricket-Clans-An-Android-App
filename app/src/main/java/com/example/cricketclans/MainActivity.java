5f1c04eb5d06a79a6a6ed9788eac5013ea12b40e
AsifAli119 <abubakqar119@gmail.com>
2023-05-22 14:26:26 +0530

    two files ' main.xml and main.java' modified

--- app/src/main/java/com/example/cricketclans/MainActivity.java
+++ app/src/main/java/com/example/cricketclans/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.cricketclans;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button regs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button regs = findViewById(R.id.regs);
+        regs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("The button is pressed");
+            }
+        });
     }
 }
